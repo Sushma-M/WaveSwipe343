58904ffeab1f2b972e127a689e5cc756e54acd96
Vcs  User <sushma.vcsuser@wavemaker.com>
2018-10-16 13:05:38 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "mobile",
+  "defaultLanguage" : "en",
+  "displayName" : "WaveSwipe343",
+  "homePage" : "Main",
+  "name" : "WaveSwipe343",
+  "platformType" : "MOBILE",
+  "securityEnabled" : "false",
+  "supportedLanguages" : "en",
+  "type" : "APPLICATION",
+  "version" : "1.0",
+  "xsrf_header_name" : "X-WM-XSRF-TOKEN"
+};
